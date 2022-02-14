846f6915433d6419267cef9e3d5d6bc1a8f0dbe8
Stanley Pan <imoment.developer@gmail.com>
2022-02-14 15:31:42 -0500

    Initial Commit

---
+++ AnalyticsBarGraph/AnalyticsBarGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsBarGraphApp.swift
+//  AnalyticsBarGraph
+//
+//  Created by Stanley Pan on 2022/02/14.
+//
+
+import SwiftUI
+
+@main
+struct AnalyticsBarGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
